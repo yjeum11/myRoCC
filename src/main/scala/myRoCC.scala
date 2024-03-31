c406f89d8059fefc8a47a486aec1be3af7f88b4b
YJ Eum <yjeum@cmu.edu>
2024-03-31 14:41:16 -0400

    multiplier complete

--- src/main/scala/myRoCC.scala
+++ src/main/scala/myRoCC.scala
@@ -1,12 +1,17 @@
+package myRoCC
+
 import chisel3._
 import chisel3.util._
 import freechips.rocketchip.tile._
 import org.chipsalliance.cde.config._
+import freechips.rocketchip._
 import freechips.rocketchip.diplomacy._
 
 class WithmyRoCC extends Config((site, here, up) => {
-  case BuildRoCC => Seq((p: Parameters) => LazyModule(
-    new myRoCC(OpcodeSet.custom0(p))))
+  case BuildRoCC => Seq((p: Parameters) => {
+    val myrocc = LazyModule.apply(new myRoCC(OpcodeSet.custom0)(p))
+    myrocc
+  })
 })
 
 class myRoCC(opcodes: OpcodeSet)
@@ -30,8 +35,10 @@ class myRoCCModule(outer: myRoCC)
   // rs1 - the value of source register 1
   // rs2 - the value of source register 2
   val result = cmd.bits.rs1 * cmd.bits.rs2
+  cmd.ready := true.B
   io.busy := false.B
   io.resp.bits.rd := cmd.bits.inst.rd
   io.resp.bits.data := result
+  io.resp.valid := cmd.valid
   io.interrupt := false.B
 }
