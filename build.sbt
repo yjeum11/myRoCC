e87737b318bde82b3ae58e7515ae607644c303ee
YJ Eum <yjeum@cmu.edu>
2024-06-26 14:34:04 -0400

    2 bit cin and cout. can implement add,sub,mul

--- build.sbt
+++
@@ -1,3 +0,0 @@
-version := "1.0"
-name := "myRoCC"
-scalaVersion := "2.13.10"
--- src/main/scala/myRoCCCarry.scala
+++ src/main/scala/myRoCCCarry.scala
@@ -21,118 +21,163 @@ class myRoCCCarry(opcodes: OpcodeSet)
 
 class myRoCCCarryModule(outer: myRoCCCarry)
     extends LazyRoCCModuleImp(outer) {
-  //val cmd = Queue(io.cmd)
-  //
-  // The parts of the command are as follows
-  // inst - the parts of the instruction itself
-  //   opcode
-  //   rd - destination register number
-  //   rs1 - first source register number
-  //   rs2 - second source register number
-  //   funct
-  //   xd - is the destination register being used?
-  //   xs1 - is the first source register being used?
-  //   xs2 - is the second source register being used?
-  // rs1 - the value of source register 1
-  // rs2 - the value of source register 2
-  //
-  // Idea: we only need adcqc + 2 3-bit fields to select cin and cout
-  // conditional execution also needs 2 3-bit fields to select cout and ctrl
-  // we also need cadcq
-  // Carry clear instruction
-  //
-  // 7 bits of funct. 
-  // funct[6] bit is for add/sub, funct[5:3] is flag1, funct[2:0] is flag2
-  //
-  // custom0 - 0 adcqc (unconditional cin and cout)
-  //         - 1 sbcqc
-  // custom1 - 0 caddqc (conditional cout)
-  //         - 1 csubqc
-  // custom2 - 0 cadcq (conditional cin)
-  //         - 1 csbcq
-  //         still need not conditional. Inc/decrement
-  //         Set carry flag. I-type instruction?
-  // custom3 - read/write flags
-  //           MSB 1 read, 0 write
-  //           bottom 3 bits is flag
-  //
+
   val opcode = io.cmd.bits.inst.opcode
 
-  val cin = Wire(Bool())
-  cin := false.B
-  val cin_index = io.cmd.bits.inst.funct(5, 3)
-  val cout_index = io.cmd.bits.inst.funct(2, 0)
-  val addsub = io.cmd.bits.inst.funct(6)
+  val cin_index = io.cmd.bits.inst.funct(3, 2)
+  val cout_index = io.cmd.bits.inst.funct(1, 0)
 
-  val result = Wire(UInt(65.W))
-  result := 0.U
-  val cout = result(64)
-  val sum = result(63, 0)
+  val regs = RegInit(VecInit(Seq.fill(4)(false.B)))
 
-  val regs = RegInit(VecInit(Seq.fill(8)(false.B)))
+  val funct7 = io.cmd.bits.inst.funct
 
   val dont_resp = Wire(Bool())
   dont_resp := false.B
 
-  when (opcode === "b0001011".U) { // custom0
-    when (addsub === 1.U) {
-      result := io.cmd.bits.rs1 -& io.cmd.bits.rs2 - cin
-    } .otherwise {
-      result := io.cmd.bits.rs1 +& io.cmd.bits.rs2 + cin
-    }
+  val sum = Wire(UInt(65.W))
+  sum := 0.U
 
-    when (io.cmd.fire) {
-      when (cout_index =/= 0.U) {
-        regs(cout_index) := cout
-      }
-    } 
-    when (cin_index =/= 0.U) {
-      cin := regs(cin_index)
-    } 
+  io.resp.bits.data := 0.U
 
-    io.resp.bits.data := sum
-
-  } .elsewhen (opcode === "b0101011".U) { //custom1
-    // conditional cout
-    val cond = regs(cin_index)
-    
-    when (io.cmd.fire) {
-      when (cout_index =/= 0.U) {
-        regs(cout_index) := Mux(cond, cout, false.B);
-      }
-      when (addsub === 1.U) {
-        result := Mux(cond, io.cmd.bits.rs1 -& io.cmd.bits.rs2, io.cmd.bits.rs1)
+  when (io.cmd.fire) {
+    when (opcode === "b0001011".U) { // custom0
+      when (funct7(6) === 0.U) { // addq
+        when (funct7(5, 4) === 0.U) {
+          sum := io.cmd.bits.rs1 +& io.cmd.bits.rs2
+        } .elsewhen (funct7(5, 4) === 1.U) { // addqc
+          sum := io.cmd.bits.rs1 +& io.cmd.bits.rs2
+          regs(cout_index) := sum(64)
+        } .elsewhen (funct7(5, 4) === 2.U) { // adcq
+          sum := io.cmd.bits.rs1 +& io.cmd.bits.rs2 +& regs(cin_index)
+        } .otherwise { // adcqc
+          sum := io.cmd.bits.rs1 +& io.cmd.bits.rs2 +& regs(cin_index)
+          regs(cout_index) := sum(64)
+        }
       } .otherwise {
-        result := Mux(cond, io.cmd.bits.rs1 +& io.cmd.bits.rs2, io.cmd.bits.rs1)
+        when (funct7(5, 4) === 0.U) {
+          sum := io.cmd.bits.rs1 -& io.cmd.bits.rs2
+        } .elsewhen (funct7(5, 4) === 1.U) { // subqc
+          sum := io.cmd.bits.rs1 -& io.cmd.bits.rs2
+          regs(cout_index) := sum(64)
+        } .elsewhen (funct7(5, 4) === 2.U) { // sbcq
+          sum := io.cmd.bits.rs1 -& io.cmd.bits.rs2 -& regs(cin_index)
+        } .otherwise { // sbcqc
+          sum := io.cmd.bits.rs1 -& io.cmd.bits.rs2 -& regs(cin_index)
+          regs(cout_index) := sum(64)
+        }
       }
-    } 
-    io.resp.bits.data := sum
-    
-  } .elsewhen (opcode === "b1011011".U) { //custom2
-    // conditional cin
-    //
-    val cond = regs(cout_index)
-
-    when (cin_index =/= 0.U) {
-      cin := regs(cin_index)
-    } 
-    
-    when (io.cmd.fire) {
-      when (addsub === 1.U) {
-        result := Mux(cond, io.cmd.bits.rs1 -& io.cmd.bits.rs2 + cin, io.cmd.bits.rs1)
+      io.resp.bits.data := sum(63, 0)
+    } .elsewhen (opcode === "b0101011".U) { //custom1 conditional ops
+      when (funct7(6) === 0.U) { // addq
+        when (funct7(5, 4) === 0.U) {
+          // cin, non inverted
+          val cond = regs(cout_index)
+          sum := Mux(cond, io.cmd.bits.rs1 +& io.cmd.bits.rs2 +& regs(cin_index), io.cmd.bits.rs1)
+        } .elsewhen (funct7(5, 4) === 1.U) { 
+          // cin, inverted
+          val cond = regs(cout_index)
+          sum := Mux(~cond, io.cmd.bits.rs1 +& io.cmd.bits.rs2 +& regs(cin_index), io.cmd.bits.rs1)
+        } .elsewhen (funct7(5, 4) === 2.U) {
+          // cout, non inverted
+          val cond = regs(cin_index)
+          sum := Mux(cond, io.cmd.bits.rs1 +& io.cmd.bits.rs2, io.cmd.bits.rs1)
+          regs(cout_index) := sum(64)
+        } .otherwise { // adcqc
+          // cout, inverted
+          val cond = regs(cin_index)
+          sum := Mux(~cond, io.cmd.bits.rs1 +& io.cmd.bits.rs2, io.cmd.bits.rs1)
+          regs(cout_index) := sum(64)
+        }
       } .otherwise {
-        result := Mux(cond, io.cmd.bits.rs1 +& io.cmd.bits.rs2 - cin, io.cmd.bits.rs1)
+        when (funct7(5, 4) === 0.U) {
+          // cin, non inverted
+          val cond = regs(cout_index)
+          sum := Mux(cond, io.cmd.bits.rs1 -& io.cmd.bits.rs2 -& regs(cin_index), io.cmd.bits.rs1)
+        } .elsewhen (funct7(5, 4) === 1.U) { 
+          // cin, inverted
+          val cond = regs(cout_index)
+          sum := Mux(~cond, io.cmd.bits.rs1 -& io.cmd.bits.rs2 -& regs(cin_index), io.cmd.bits.rs1)
+        } .elsewhen (funct7(5, 4) === 2.U) {
+          // cout, non inverted
+          val cond = regs(cin_index)
+          sum := Mux(cond, io.cmd.bits.rs1 -& io.cmd.bits.rs2, io.cmd.bits.rs1)
+          regs(cout_index) := sum(64)
+        } .otherwise { // adcqc
+          // cout, inverted
+          val cond = regs(cin_index)
+          sum := Mux(~cond, io.cmd.bits.rs1 -& io.cmd.bits.rs2, io.cmd.bits.rs1)
+          regs(cout_index) := sum(64)
+        }
       }
-    } 
-    io.resp.bits.data := sum
-  } .elsewhen (opcode === "b1111011".U) { //custom3
-    when (addsub === 1.U) { // read from carry
-      io.resp.bits.data := regs(cout_index)
-    } .otherwise {
-      // don't respond at all
-      dont_resp := true.B
-      io.resp.bits.data := 0.U
-      regs(cout_index) := io.cmd.bits.rs1(0)
+      io.resp.bits.data := sum(63, 0)
+    } .elsewhen (opcode === "b1011011".U) { //custom2
+      val code = funct7(6, 4)
+      when (code === 0.U) { // move
+          dont_resp := true.B
+          regs(cout_index) := regs(cin_index)
+      } .elsewhen (code === 1.U) { //mulhi
+          val mul_res = Wire(UInt(128.W))
+
+          mul_res := io.cmd.bits.rs1 * io.cmd.bits.rs2
+          io.resp.bits.data := mul_res(127, 64)
+      } .elsewhen (code === 2.U) { // mullo
+          val mul_res = Wire(UInt(128.W))
+
+          mul_res := io.cmd.bits.rs1 * io.cmd.bits.rs2
+          io.resp.bits.data := mul_res(63, 0)
+      } .elsewhen (code === 3.U) { // cdecq
+          dont_resp := true.B
+          regs(cout_index) := regs(cin_index) - regs(cout_index)
+      }
+    } .elsewhen (opcode === "b1111011".U) { //custom3
+      val code = funct7(6, 4)
+      when (code === 0.U) {
+        // load from carry reg
+        io.resp.bits.data := regs(cout_index)
+      } .elsewhen (code === 1.U) {
+        // store to carry reg
+        dont_resp := true.B
+        regs(cout_index) := io.resp.bits.data(0)
+      } .elsewhen (code === 2.U) {
+        // czero
+        when (regs(cout_index) === 1.U) {
+          io.resp.bits.data := 0.U
+        } .otherwise {
+          io.resp.bits.data := io.cmd.bits.rs1
+        }
+      } .elsewhen (code === 3.U) {
+        // cncmplq
+        val cond = regs(cout_index)
+        dont_resp := true.B
+        when (cond === 0.U) {
+          regs(cin_index) := io.cmd.bits.rs1 < io.cmd.bits.rs2
+        }
+      } .elsewhen (code === 4.U) {
+        // cncmpeq
+        val cond = regs(cout_index)
+        dont_resp := true.B
+        when (cond === 0.U) {
+          regs(cin_index) := io.cmd.bits.rs1 === io.cmd.bits.rs2
+        }
+      } .elsewhen (code === 5.U) {
+        // cncmpgq
+        val cond = regs(cout_index)
+        dont_resp := true.B
+        when (cond === 0.U) {
+          regs(cin_index) := io.cmd.bits.rs1 > io.cmd.bits.rs2
+        }
+      } .elsewhen (code === 6.U) {
+        // cncmpneq
+        val cond = regs(cout_index)
+        dont_resp := true.B
+        when (cond === 0.U) {
+          regs(cin_index) := io.cmd.bits.rs1 =/= io.cmd.bits.rs2
+        }
+      } .otherwise {
+        // andb
+        dont_resp := true.B
+        regs(cout_index) := regs(cin_index) & regs(cout_index)
+      }
     }
   }
 
